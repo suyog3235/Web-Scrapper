41c19d9803076571a1b9a56a2f9196776821d77c
suyog kulkarni <65725023+suyog3235@users.noreply.github.com>
2021-09-06 21:47:24 +0530

    updated
    changed some stuff in main.py

--- main.py
+++ main.py
@@ -8,16 +8,17 @@ from bs4 import BeautifulSoup
 def facebook():
 
     # facebook login data
-    USERNAME = 'Youremailid@gmail.com // username'
-    PASSWORD = 'your password'
+    USERNAME = 'Your facebook username'
+    PASSWORD = 'your facebook password'
 
     # turn off all the facebook popup notifications
     chrome_options = webdriver.ChromeOptions()
     prefs = {"profile.default_content_setting_values.notifications": 2}
     chrome_options.add_experimental_option("prefs", prefs)
-    
-    #FOR EXAMPLE  driver = webdriver.Chrome(options=chrome_options, executable_path=r"C:\Users\XD\Desktop\internship program\chromedriver.exe")
-    driver = webdriver.Chrome(options=chrome_options, executable_path=r"CHROME DRIVER PATH")
+
+    # FOR EXAMPLE  driver = webdriver.Chrome(options=chrome_options, executable_path=r"C:\Users\XD\Desktop\internship program\chromedriver.exe")
+    driver = webdriver.Chrome(options=chrome_options,
+                              executable_path=r"ADD CHROME DRIVER PATH HERE SEE EXAMPLE ABOVE")
     driver.maximize_window()
 
     # login to face book (using m basic here because its easier to scrape with mbasic )
@@ -37,7 +38,8 @@ def facebook():
     while True:
 
         # Scroll down to the bottom.
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
 
         # Wait to load the page.
         time.sleep(5)
@@ -74,7 +76,7 @@ def facebook():
                             total_fb_comments_in_july += 1
     except Exception as e:
         print("error while getting data from facebook !!!!")
-        print("Error:",e)
+        print("Error:", e)
 
     print("success !!!")
     wb = load_workbook('Audit Report For DM.xlsx')
@@ -93,15 +95,16 @@ def facebook():
     driver.close()
 
 
-def linkdin():
+def linkedin():
 
     # FOR EXAMPLE:  driver = webdriver.Chrome(r"C:\Users\XD\Desktop\internship program\chromedriver.exe")
-    driver = webdriver.Chrome(r" chromedriver path ")
+    driver = webdriver.Chrome(
+        r"ADD CHROME DRIVER PATH HERE SEE EXAMPLE ABOVE")
     driver.maximize_window()
 
     # linkdin account info
-    USERNAME = 'youremailid@gmail.com // your username'
-    PASSWORD = 'your password'
+    USERNAME = 'your linkdin username'
+    PASSWORD = 'your linkdin password'
 
     driver.get("https://www.linkedin.com/login")
     # driver.refresh()
@@ -114,14 +117,16 @@ def linkdin():
     password.send_keys(Keys.RETURN)
     time.sleep(2)
 
-    driver.get("https://www.linkedin.com/company/cloud-counselage/posts/?feedView=all")
+    driver.get(
+        "https://www.linkedin.com/company/cloud-counselage/posts/?feedView=all")
     time.sleep(5)
 
     last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
 
         # Scroll down to the bottom.
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);")
 
         # Wait to load the page.
         time.sleep(2)
@@ -145,16 +150,19 @@ def linkdin():
 
     try:
         for post in total_posts:
-            day_time = post.find_all("span", attrs={"class": "visually-hidden"})
+            day_time = post.find_all(
+                "span", attrs={"class": "visually-hidden"})
             for d in day_time:
                 if d.text == "2 months ago":
                     total_posts_in_july += 1
 
-                    likes = post.find_all("span", attrs={'class': "v-align-middle social-details-social-counts__reactions-count"})
+                    likes = post.find_all("span", attrs={
+                                          'class': "v-align-middle social-details-social-counts__reactions-count"})
                     for like in likes:
                         total_likes += int(like.text)
 
-                    comments = post.find_all("", attrs={"class": "social-details-social-counts__comments"})
+                    comments = post.find_all(
+                        "", attrs={"class": "social-details-social-counts__comments"})
                     for comment in comments:
                         total_comments += int(comment.text)
     except Exception as e:
@@ -174,10 +182,5 @@ def linkdin():
     driver.close()
 
 
-
 facebook()
-linkdin()
-
-
-
-
+linkedin()
